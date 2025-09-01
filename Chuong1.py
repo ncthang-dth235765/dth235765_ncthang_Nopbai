fcb8c1ab8c1cb0a7ef20ec64ff8bc8ea5e573683
ncthang-dth235765 <thang_dth235765@student.agu.edu.vn>
2025-09-01 11:06:20 +0700

    Add files via upload

--- Chuong1.py
+++ Chuong1.py
@@ -1,5 +1,37 @@
-print('Chao cac ban!')
-print('Hello World!')
+print('Chao cac ban!\n')
+print('Hello World!\n')
+
+print('Albert Einstein đã từng nói: Một người không bao giờ mắc sai lầm không bao giờ thử bất kỳ điều gì mới.\n')
+
+if 5>2:
+    print('Five is greater than two!\n')
+
+total=1+\
+2+\
+3
+print(total);print('Hello Python!\n')
+
+if True:
+    print('True\n')
+else:
+    print('False\n')
+
+#This is a comment
+print('Hello, World!\n')
+
+import math
+import os
+
+import math_libs
+math_libs.add(4,5)
+
+import moudle1
+import moudle2
+print(moudle1.question)
+print(moudle2.question)
+print(moudle1.answer)
+print(moudle2.answer)
+
 print('* * * *')
 print('*     *')
 print('*     *')
